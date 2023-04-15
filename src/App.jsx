e56fcea935a0fb9d6ad71382944e33ac573aed55
alekoyunja <alikoyuncu13@gmail.com>
2023-04-15 23:49:05 +0300

    Components added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomerForm from './components/CustomerForm';
+import CustomerList from './components/CustomerList';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerForm/>
+      <CustomerList/>
+    </div>
+  );
+}
+
+export default App;
